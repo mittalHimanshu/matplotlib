729bad7e4f32ca3439bd2a1fb8062d0160f40182
Himanshu Mittal <mittalhimanshu121@gmail.com>
2019-01-07 18:46:22 +0530

    simple data visulaization

---
+++ Data-Visualization/first.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], color='green', linestyle='dashed')
+plt.plot([2, 3, 4, 5], [1, 2, 3, 4], color='red', linestyle='dashdot')
+plt.ylabel('Important Figures')
+
+plt.show()
